8b1e341a0009f2f49837595e67f0255f2778b3be
renesalomone <108975981+renesalomone@users.noreply.github.com>
2024-03-13 09:18:11 +0700

    Initial Commit

---
+++ UberCloneSwiftUI/UberCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneSwiftUIApp.swift
+//  UberCloneSwiftUI
+//
+//  Created by Rene Salomone on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
